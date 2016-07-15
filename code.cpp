b1aaa5fb09277f70257ee312bb50fa22e335fb2a
Jason Lu <jasonbangbang@gmail.com>
2016-07-15 08:06:03 -0500

    Remove using namespace std

--- code.cpp
+++ code.cpp
@@ -5,22 +5,21 @@
 #include <cstdlib>
 
 int main(int argc, const char * argv[]) {
-    using namespace std;
     std::map <std::string, std::string> accounts;
 
     // TODO: Read from a file maybe?
     accounts["test"] = "test";
     accounts["oliviawlee"] = "12345";
 
-    string username1;
-    string password1;
+    std::string username1;
+    std::string password1;
 
-    cout << " Please enter your username:"<< "\n";
-    cin >> username1;
+    std::cout << " Please enter your username:"<< "\n";
+    std::cin >> username1;
 
 
-    cout << " Please enter your password:"<< "\n";
-    cin >> password1;
+    std::cout << " Please enter your password:"<< "\n";
+    std::cin >> password1;
 
     // For this part you can always to GOTOs
     if (accounts.find(username1) == accounts.end() || accounts[username1] != password1) {
