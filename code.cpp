9728330660e27cda641d4a06b622e532f9054c7f
Jason Lu <jasonbangbang@gmail.com>
2016-07-15 08:04:20 -0500

    Add support for multiple usernames and passwords

--- code.cpp
+++ code.cpp
@@ -1,14 +1,19 @@
 //C++
 #include <iostream>
 #include <string>
+#include <map>
+#include <cstdlib>
 
 int main(int argc, const char * argv[]) {
     using namespace std;
+    std::map <std::string, std::string> accounts;
 
+    // TODO: Read from a file maybe?
+    accounts["test"] = "test";
+    accounts["oliviawlee"] = "12345";
 
     string username1;
     string password1;
-
 
     cout << " Please enter your username:"<< "\n";
     cin >> username1;
@@ -17,17 +22,10 @@ int main(int argc, const char * argv[]) {
     cout << " Please enter your password:"<< "\n";
     cin >> password1;
 
-    if (username1== "oliviawlee" && password1== "12345")
-    {
-        cout << " Access granted." << "\n";
+    // For this part you can always to GOTOs
+    if (accounts.find(username1) == accounts.end() || accounts[username1] != password1) {
+        std::cout << "Access denied" << std::endl;
+    } else if (accounts[username1] == password1) {
+        std::cout << "Access granted" << std::endl;
     }
-    else
-    {
-        cout << " Access denied." << "\n";
-
-        return 0;
-}
- // I need to find a way for the code to be able to store multiple usernames and passwords so I could possibly sign in with different accounts. Thanks!
-
- 
 }
