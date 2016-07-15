8d6cb5c8f9018d5da1d5a8494c070353f0d5dd5b
Jason Lu <jasonbangbang@gmail.com>
2016-07-15 07:47:29 -0500

    Strip whitespace

--- code.cpp
+++ code.cpp
@@ -3,22 +3,20 @@
 #include <string>
 
 int main(int argc, const char * argv[]) {
-   
     using namespace std;
-    
-    
-    
+
+
     string username1;
     string password1;
-    
-    
+
+
     cout << " Please enter your username:"<< "\n";
     cin >> username1;
 
 
     cout << " Please enter your password:"<< "\n";
     cin >> password1;
-  
+
     if (username1== "oliviawlee" && password1== "12345")
     {
         cout << " Access granted." << "\n";
@@ -26,8 +24,9 @@ int main(int argc, const char * argv[]) {
     else
     {
         cout << " Access denied." << "\n";
-   
-        return 0;}
+
+        return 0;
+}
  // I need to find a way for the code to be able to store multiple usernames and passwords so I could possibly sign in with different accounts. Thanks!
 
  
