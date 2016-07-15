6cbbf864d6943f31de320c201f4441975272b628
Jason Lu <jasonbangbang@gmail.com>
2016-07-15 08:09:06 -0500

    Document code

--- code.cpp
+++ code.cpp
@@ -5,9 +5,17 @@
 #include <cstdlib>
 
 int main(int argc, const char * argv[]) {
+    /* Maps are a way to store a 'key' and 'data'.
+     * In our case, the key is the username, and the
+     * data is the password. This is probably not the best
+     * way, but hey, it works.
+     */
     std::map <std::string, std::string> accounts;
 
     // TODO: Read from a file maybe?
+    /* To add more accounts and password, use this format:
+     * accounts["usernamehere"] = "passwordhere";
+     */
     accounts["test"] = "test";
     accounts["oliviawlee"] = "12345";
 
@@ -21,7 +29,12 @@ int main(int argc, const char * argv[]) {
     std::cout << " Please enter your password:"<< "\n";
     std::cin >> password1;
 
-    // For this part you can always to GOTOs
+    /* Here we check for invalid username or invalid passwords
+     * The first part of the if (accounts.find...) checks if the
+     * username exists (remember, the username is the key). The
+     * second part (accounts[username1] != ...) checks to see
+     * if the password matches
+     */
     if (accounts.find(username1) == accounts.end() || accounts[username1] != password1) {
         std::cout << "Access denied" << std::endl;
     } else if (accounts[username1] == password1) {
