a7d840d97340f8dcb1c1240e259de565b7a05d66
Olivia Lee <olivialovenikki@gmail.com>
2016-07-15 15:23:28 -0500

    Update code.cpp
    oliviawlee

--- code.cpp
+++ code.cpp
@@ -1,34 +1,25 @@
-//C++
+
 #include <iostream>
 #include <string>
-
+#include <map>
+#include <cstdlib>
 int main(int argc, const char * argv[]) {
-   
-    using namespace std;
-    
-    
-   
-    string username1;
-    string password1;
-    
-    
-    cout << " Please enter your username:"<< "\n";
-    cin >> username1;
-
-
-    cout << " Please enter your password:"<< "\n";
-    cin >> password1;
-  
-    if (username1== "oliviawlee" && password1== "12345")
-    {
-        cout << " Access granted." << "\n";
-    }
-    else
-    {
-        cout << " Access denied." << "\n";
-   
-        return 0;}
- // I need to find a way for the code to be able to store multiple usernames and passwords so I could possibly sign in with different accounts. Thanks!
-
  
+ std::map <std::string, std::string> accounts;
+    accounts["olivialee27"] = "NASA";
+    accounts["user101"] = "abc123";
+    accounts["ol27"] = "12345";
+    /* KEY is the username and DATA is the password. accounts["key"] = "data";
+     */
+    std::string username1;
+    std::string password1;
+    std::cout << " Please enter your username:"<< "\n";
+    std::cin >> username1;
+    std::cout << " Please enter your password:"<< "\n";
+    std::cin >> password1;
+  if (accounts.find(username1) == accounts.end() || accounts[username1] != password1) {
+      std::cout << "Access denied" << std::endl;
+    } else if (accounts[username1] == password1) {
+        std::cout << "Access granted" << std::endl;
+        return 0;   }
 }
